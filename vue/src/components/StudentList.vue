ae5df01405fcd8981c367c1ebb4d4bcb99b7a066
Illeas <illeashossain34@gmail.com>
2022-08-07 06:55:00 -0400

    Get data
    Get student data using API from the student table

--- vue/src/components/StudentList.vue
+++ vue/src/components/StudentList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="contaner">
+    <div class="container">
         <table class="table table-hove">
             <thead>
                 <tr>
@@ -10,13 +10,13 @@
                     <th scope="col">Contact Number</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody v-for="studentlist in studentlist" :key="studentlist.id">
                 <tr class="table-secondary">
-                    <th scope="row">1</th>
-                    <th scope="row">1</th>
-                    <th scope="row">1</th>
-                    <th scope="row">1</th>
-                    <th scope="row">1</th>
+                    <th scope="row">{{ studentlist.id }}</th>
+                    <th scope="row">{{ studentlist.class }}</th>
+                    <th scope="row">{{ studentlist.name }}</th>
+                    <th scope="row">{{ studentlist.email_address }}</th>
+                    <th scope="row">{{ studentlist.contact_number }}</th>
                 </tr>
             </tbody>
         </table>
@@ -30,7 +30,26 @@ import axios from 'axios';
     export default{
         name: 'StudentList',
         data(){
-            
+            return{
+                studentlist: Array
+            }
+        }, 
+        created(){
+            this.getStudent();
+        },
+        methods: {
+            async getStudent(){
+                let url = 'http://127.0.0.1:8000/api/student';
+                await axios.get(url).then(response => {
+                    this.studentlist = response.data.student;
+
+                }).catch(error => {
+                    console.log(error);
+                });
+            }
+        },
+        mounted(){
+            console.log('Student list component mounted!');
         }
     }
 </script>
